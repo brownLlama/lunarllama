d4094c52c269fcd6965e9358af3ba45b6a24d470
Sanu Maharjan <inbox.sanu@gmail.com>
2023-07-03 22:09:45 +0200

    Create mason.lua

---
+++ lua/lunarllama/plugins/lsp/mason.lua
@@ -0,0 +1,47 @@
+    return
+end
+
+-- enable mason
+mason.setup()
+
+mason_lspconfig.setup({
+    -- list of servers for mason to install
+    ensure_installed = {
+        "lua_ls",
+        "pyright", -- python
+        "dockerls", -- docker
+        "docker_compose_language_service", -- docker compose
+        -- "sqlls", -- sql
+        "yamlls", -- YAML
+        "terraformls", -- terraform
+        "tflint", -- terraform
+        "jsonls", -- json
+        "remark_ls", -- markdown
+        "bashls", -- bash
+        "grammarly", -- grammarly
+    },
+    -- auto-install configured servers (with lspconfig)
+    automatic_installation = true, -- not the same as ensure_installed
+})
+
+mason_null_ls.setup({
+    -- list of formatters & linters for mason to install
+    ensure_installed = {
+        "prettier", -- ts/js formatter
+        "stylua", -- lua formatter
+        "black", -- python
+        "pydocstyle", -- python
+        "isort", -- python
+        "flake8", -- python
+        "sqlfmt", -- sql
+        "yamlfix", -- YAML
+        "yamlfmt", -- YAML
+        "terraform_fmt", -- terraform
+        "terraform_validate", -- terraform
+        "jsonlint", -- json
+        "hadolint", -- Dockerfile
+        "markdownlint", -- markdown
+    },
+    -- auto-install configured formatters & linters (with null-ls)
+    automatic_installation = true,
+})
