452180b6ae657c71ce00f796cad06d6376555e8c
brownLlama <inbox.sanu@gmail.com>
2023-07-10 20:46:03 +0200

    move command update1

--- prep.sh
+++ prep.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 mkdir .config && cd .config || exit
-mkdir lvim && cd lvim || exit
+mkdir lvim
 cd || exit
-mv lunarllama/{..}* .config/lvim/
+mv lunarllama/{.,}*(D) .config/lvim/
 rm -rf 1lunarllama
 
 sudo apt update
