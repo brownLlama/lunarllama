2aad9957d20fc01346708f0e336d3daf2c1330c9
brownLlama <inbox.sanu@gmail.com>
2023-07-10 12:51:47 +0200

    new file structure

--- step3.sh
+++ step3.sh
@@ -13,7 +13,7 @@ echo "export PATH=/home/sanu_maharjan/.local/bin:$PATH" >> ~/.zshrc
 echo 'alias lvim="sudo -E env \"PATH=$PATH\" /home/sanu_maharjan/.local/bin/lvim"' >> ~/.zshrc
 source ~/.zshrc
 
-cd .config
-git clone https://github.com/brownLlama/lunarllama.git
-cp -R lunarllama/. lvim/
-rm -rf lunarllama
+# cd .config
+# git clone https://github.com/brownLlama/lunarllama.git
+# cp -R lunarllama/. lvim/
+# rm -rf lunarllama
