c77357574fb581c99758ffa0bf2f6a05f2d9d2de
brownLlama <inbox.sanu@gmail.com>
2023-07-10 20:41:12 +0200

    two line command structure created

--- prep.sh
+++ prep.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-mv .config/lvim/lunarllama/* .config/lvim/
-mv .config/lvim/lunarllama/.git .config/lvim/
-rm -rf .config/lvim/lunarllama
+mkdir .config && cd .config || exit
+mkdir lvim && cd lvim || exit
 cd || exit
+mv lunarllama/{..}* .config/lvim/
+rm -rf 1lunarllama
 
 sudo apt update
 sudo apt install zsh
