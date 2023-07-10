45812da8b5173250cdf20c3a9c7acd1c9919de5f
brownLlama <inbox.sanu@gmail.com>
2023-07-10 14:11:25 +0200

    source is changed to .

--- step3.sh
+++ step3.sh
@@ -4,7 +4,7 @@ python3.11 -m pip install --upgrade pip
 
 echo "export PATH=/home/sanu_maharjan/.local/bin:$PATH" >> ~/.zshrc
 echo 'alias lvim="sudo -E env \"PATH=$PATH\" /home/sanu_maharjan/.local/bin/lvim"' >> ~/.zshrc
-source ~/.zshrc
+. ~/.zshrc
 
 # cd .config
 # git clone https://github.com/brownLlama/lunarllama.git
